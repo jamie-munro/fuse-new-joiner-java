1b840b98fc2df1eb723e50f47f15695f03c569a8
Jamie Munro <jbm@jbm.fyi>
2022-10-17 18:31:05 +0100

    Both endpoints for IEX feignClient updated to follow the new API format (requiring token). Wiremocks for both ref-data/symbols and tops/last updated to follow new IEX API format (requiring token). All IexRestControllerTest tests passed.

--- src/main/java/org/galatea/starter/service/IexClient.java
+++ src/main/java/org/galatea/starter/service/IexClient.java
@@ -20,7 +20,7 @@ public interface IexClient {
    *
    * @return a list of all of the stock symbols supported by IEX.
    */
-  @GetMapping("/ref-data/symbols")
+  @GetMapping("/ref-data/symbols?token=${spring.rest.iexToken}")
   List<IexSymbol> getAllSymbols();
 
   /**
@@ -29,7 +29,7 @@ public interface IexClient {
    * @param symbols stock symbols to get last traded price for.
    * @return a list of the last traded price for each of the symbols passed in.
    */
-  @GetMapping("/tops/last")
+  @GetMapping("/tops/last?token=${spring.rest.iexToken}")
   List<IexLastTradedPrice> getLastTradedPriceForSymbols(@RequestParam("symbols") String[] symbols);
 
 }
